d08e0d436c952643b02fb8cd66d46f710b0b4851
Venus Garg <garg.venus39@gmail.com>
2016-12-20 07:14:44 +0000

    Passport files

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,12 +2,7 @@ var express = require('express');
 var router = express.Router();
 //var User = require('./model/users');
 
-// localhost:8080/users?id=dali&pass=dali
-
-router.post('/', function(req, res) {
-    username = req.query.username;
-    password =  req.query.password;
-});
+// localhost:8080/users?id=venus&pass=venus
 
 router.post('/', passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) {
 	username = req.query.username;
